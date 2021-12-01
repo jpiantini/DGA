bb28b7f83f5a3e98530c99d4109da124e20f964a
“Starlyn” <starlynfernandez1222@hotmail.com>
2021-12-01 17:57:19 -0400

    Working CarouselBootrap

--- src/views/Home/components/carrosuel/CarouselBootstrap.js
+++ src/views/Home/components/carrosuel/CarouselBootstrap.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Carousel from "react-bootstrap/Carousel";
+import { Carousel } from "react-bootstrap";
 
 import "./Style/style.css";
 import { HomeTextContainer, HomeTitle } from "../../styles/HomeStyles";
