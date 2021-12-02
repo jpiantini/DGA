89da0e87d86a14cf5f2fc2cfa883da460541bc04
yayidg22 <yayidg2010@gmail.com>
2021-12-02 15:38:30 -0800

    some new enhancements added register now is working

---
+++ src/views/Auth/RegisterNew/styles/RegisterNewStyles.js
@@ -0,0 +1,56 @@
+import { styled } from '@mui/system';
+import COLORS from '../../../../theme/Colors';
+
+export const Container = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+});
+
+export const ButtonsContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between'
+});
+
+export const ButtonContainer = styled('div')({
+    width: '30%',
+});
+
+export const PaymentMethodsContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between'
+
+
+});
+
+export const ImageContainer = styled('div')({
+    position:'relative',
+    display:'flex',
+    flexDirection:'column',
+    width: '100%',
+    minHeight:'250px',
+    border:` 1px solid ${COLORS.secondary}`,
+    alignItems:'center',
+    justifyContent:'center',
+    cursor : 'pointer',
+    '@media (min-width:320px)': {
+        width: '100%',
+    },
+    '@media (min-width:768px)': {
+        width: '100%',
+    },
+    '@media (min-width:1200px)': {
+        width: '350px',
+    },
+    '&:hover':{
+        boxShadow: '0 8px 10px 0 rgba(0, 0, 0, 0.4), 0 6px 20px 0 rgba(0, 0, 0, 0.30)',
+        borderColor: COLORS.primary,
+    }
+});
+
+export const LogoImage = styled('img')({
+    width: '80%',
+});
