5228bb156f333ba6a44696ad443c468cdd25f511
yayidg22 <yayidg2010@gmail.com>
2022-03-29 17:15:47 -0700

    new enhancements added in Messages

--- src/api/ActionRequired.js
+++ src/api/ActionRequired.js
@@ -4,6 +4,10 @@ export const sendRequiredAction = (requestData) => {
     return apiServiceCall().post('/requests/action/save',requestData).then(({data}) => data);
 }
 
+export const sendMessage = (requestData) => {
+    return apiServiceCall().post('/comment',requestData).then(({data}) => data);
+}
+
 export const assingDocumentsForRequiredActionInSoftExpert = (requestData) => {
     return apiServiceCall().post('/assigndocumentsoftexpert',requestData).then(({data}) => data);
 }
